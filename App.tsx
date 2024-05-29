337c22d90eff524a25bc96d1d0fdf7c4c09e742d
bezbrain <belloquadriolawale@gmail.com>
2024-05-29 20:25:52 +0100

    Install bottom tab

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { HomeComponent } from "./src/components/screenComponents";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeComponent} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
