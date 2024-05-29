e428f99c42669e3e11da030662b1bcbdc497322c
bezbrain <belloquadriolawale@gmail.com>
2024-05-29 20:28:32 +0100

    Remove drawer navigation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { HomeComponent } from "./src/components/screenComponents";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={HomeComponent} />
-      </Drawer.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeComponent} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
