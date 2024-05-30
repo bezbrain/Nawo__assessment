fc751c8425289af50b73509ae023752c7249ca5e
bezbrain <belloquadriolawale@gmail.com>
2024-05-30 08:18:25 +0100

    Display the app splashscreen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import "react-native-gesture-handler";
+import { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet } from "react-native";
 
 import { TabNav } from "./src/screens";
 import { Colors } from "./src/utils/colors";
+import { SplashScreen } from "./src/components/helpers";
 
 const { purple600 } = Colors;
 
 export default function App() {
+  const [isAppLoading, setIsAppLoading] = useState(true);
+
+  // DISPLAY SPLASH SCREEN FOR 2 SECONDS TILL APP IS READY
+  useEffect(() => {
+    setTimeout(() => {
+      setIsAppLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isAppLoading) {
+    return (
+      <>
+        <StatusBar style="light" />
+        <SplashScreen />
+      </>
+    );
+  }
+
   return (
     <>
       <SafeAreaView style={styles.container}>
---
+++ src/components/helpers/splashScreen.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+import { Colors } from "../../utils/colors";
+
+const { purple600 } = Colors;
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.directImageCon}>
+        <View style={styles.image1Container}>
+          <View style={styles.overlay}></View>
+          <Image
+            source={require("../../../assets/splash1.png")}
+            style={styles.image1}
+          />
+        </View>
+        <Image
+          source={require("../../../assets/splash2.png")}
+          style={styles.image2}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: purple600,
+  },
+  directImageCon: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image1Container: {
+    position: "relative",
+  },
+  overlay: {
+    position: "absolute",
+    // borderWidth: 2,
+    height: 136,
+    width: 136,
+    borderRadius: 60,
+    zIndex: 10,
+    backgroundColor: "rgba(44, 20, 221, 0.8)",
+  },
+  image1: {
+    height: 136,
+    width: 136,
+  },
+  image2: {
+    marginLeft: -100,
+    zIndex: 20,
+  },
+});
